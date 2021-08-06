3bc636354d7687b1ff0ba3ee3021ffa9db3320c2
CristianTura <cristianbuenaventura15@gmail.com>
2021-08-06 14:28:19 -0500

    Adicionando responsive para mobile

--- app.js
+++ app.js
@@ -4,11 +4,52 @@ const slider = document.querySelector('.slider');
 const arrowLeft = document.querySelector('#left-arrow')
 const arrowRight = document.querySelector('#right-arrow')
 
+// ----Leer en que archivo HTML se encuentra----
+function filename(){
+    let rutaAbsoluta = self.location.href;   
+    let posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+    let rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
+    return rutaRelativa;  
+}
 
-arrowRight.addEventListener('click', ()=> {
-    slider.scrollLeft += slider.offsetWidth
+if (filename == 'index.html'){
+
+    arrowRight.addEventListener('click', ()=> {
+        slider.scrollLeft += slider.offsetWidth
+    })
+    
+    arrowLeft.addEventListener('click', ()=> {
+        slider.scrollLeft -= slider.offsetWidth
+    })
+}
+
+// ----------------Menú desplegable--------------
+
+const burger = document.querySelector('.burger')
+const close = document.querySelector('#close')
+const header = document.querySelector('#header')
+const main = document.querySelector('#main')
+const footer = document.querySelector('#footer')
+const menuElem = document.querySelectorAll('.nav-link')
+
+burger.addEventListener('click', () => {
+    header.classList.add('visible')
+    main.classList.add('visible')
+    footer.classList.add('visible')
+})
+close.addEventListener('click', () => {
+    header.classList.remove('visible')
+    main.classList.remove('visible')
+    footer.classList.remove('visible')
 })
 
-arrowLeft.addEventListener('click', ()=> {
-    slider.scrollLeft -= slider.offsetWidth
-})
+for (let i=0; i<menuElem.length; i++) {
+    menuElem[i].addEventListener('click', () => {
+        console.log('click')
+        header.classList.remove('visible')
+        main.classList.remove('visible')
+        footer.classList.remove('visible')
+    })
+}
+
+
